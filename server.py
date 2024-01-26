1b075f31a5d4aa0a56064ddd96b90211184b2320
frank chen <frankchen@astri.org>
2024-01-26 07:07:50 +0000

    update server.py and server_whoami.py

--- server.py
+++ server.py
@@ -3,59 +3,46 @@ import selectors
 
 sel = selectors.DefaultSelector()
 
-def accept(sock: socket.socket, sel: selectors.BaseSelector, mask):
-    conn, addr = sock.accept()  # Should be ready
+def accept(server_sock: socket.socket, sel: selectors.BaseSelector, mask):
+    # conn: connection socket
+    conn, addr = server_sock.accept()  # Should be ready
     print(f"Accept - conn[{conn}] from addr[{addr}]\n")
     conn.setblocking(False)
-    sel.register(conn, selectors.EVENT_READ, {"type": "data_socket"})
+    sel.register(conn, selectors.EVENT_READ, {"type": "connection_socket"})
 
 def read(conn: socket.socket, sel: selectors.BaseSelector, mask):
     data = conn.recv(1024)  # Should be ready
     addr = conn.getpeername()
     print(f"Read - data[{repr(data)}] in conn[{conn}] from addr[{addr}]\n")
     if data:
-        message =(
-            "HTTP/1.1 200 OK\r\n",
-            "Content-type: text/html\r\n",
-            "Server:localhost\r\n\r\n",
-            f"""<!doctype html>
-                <html>
-                    <body>
-                        <h1>Welcome to the server!</h1>
-                        <h2>Server address: {HOST}:{PORT}</h2>
-                        <h3>You're connected through address: {addr[0]}:{addr[1]}</h3>
-                        <body>
-                            <pre>{data.decode("utf-8")}<pre>
-                        </body>
-                    </body>
-                </html>""")
-        message = "".join(message)
-        conn.send(message.encode("utf8"))  # Hope it won't block
+        conn.sendall(data)  # Hope it won't block
     else:
+        # EOF: connection is closed by client
         print(f'Close - conn[{conn}] from addr[{addr}]\n')
         sel.unregister(conn)
         conn.close()
 
-
 HOST = "127.0.0.1"
 PORT = 3234
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind((HOST, PORT))
-sock.listen()
-sock.setblocking(False)
+# server_sock: listening socket
+server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_sock.bind((HOST, PORT))
+server_sock.listen()
+server_sock.setblocking(False)
 
-sel.register(sock, selectors.EVENT_READ, {"type": "listening_socket"})
+sel.register(server_sock, selectors.EVENT_READ, {"type": "listening_socket"})
 
 while True:
+    print(f"Select register - file descriptors(fds):{list(sel.get_map())}\n")
     events = sel.select()
     for key, mask in events:
-        print(f"Select - fileobj[{key.fileobj}] fd[{key.fd}], events[{key.events}], data[{key.data}]\n")
+        print(f"Select ready - fileobj[{key.fileobj}] fd[{key.fd}], events[{key.events}], data[{key.data}]\n")
         if key.data["type"] == "listening_socket":
             # Handle listening socket
             accept(key.fileobj, sel, mask)
-        elif key.data["type"] == "data_socket":
-            # Handle data socket
+        elif key.data["type"] == "connection_socket":
+            # Handle connection socket
             read(key.fileobj, sel, mask)
         else:
             pass
